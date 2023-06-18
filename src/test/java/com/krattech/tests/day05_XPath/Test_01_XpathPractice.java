1be1539db8107e73e3dbf6800afc8b2581ea30ed
ihsan <ihsanakcair@gmail.com>
2023-06-18 22:53:17 +0300

    Xpath and css

--- src/test/java/com/krattech/tests/day05_XPath/Test_01_XpathPractice.java
+++ src/test/java/com/krattech/tests/day05_XPath/Test_01_XpathPractice.java
@@ -6,18 +6,61 @@ import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class Test_01_XpathPractice {
-    public static void main(String[] args) {
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+    static WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+    public static void main(String[] args) throws InterruptedException {
+
+        /**
+         * navigate to https://demowebshop.tricentis.com/desktops
+         * click on Simple Computer Add to cart button by using item name in the locator
+         * close driver
+         */
+
+
         driver.manage().window().setPosition(new Point(-1000, 0));
         driver.manage().window().maximize();
 
+        driver.get("https://demowebshop.tricentis.com/desktops");
 
-        driver.get("https://www.krafttechexlab.com/login");
+        Thread.sleep(2000);
 
-        WebElement form = driver.findElement(By.xpath("//form[@class='row g-3 needs-validation']"));
-        System.out.println("form.getText() = " + form.getText());
+        //navigateTabMenu("Jewelry");
 
+        Thread.sleep(2000);
+
+      //  addToCart("Create Your Own Jewelry");
+
+        navigateAndAdd("Jewelry","Create Your Own Jewelry");
+
+        Thread.sleep(2000);
+
+        driver.close();
 
     }
+
+    public static void addToCart(String productName) throws InterruptedException {
+        WebElement product = driver.findElement(By.xpath("//a[text()='" + productName + "']/ancestor::div[@*='details" +
+                "']//input"));
+        product.click();
+        Thread.sleep(2000);
+    }
+
+    public static void navigateTabMenu(String tabName){
+        driver.findElement(By.partialLinkText(tabName)).click();
+
+    }
+
+
+    public static void navigateAndAdd(String tabName, String productName) throws InterruptedException {
+        driver.findElement(By.partialLinkText(tabName)).click();
+        Thread.sleep(2000);
+        WebElement product = driver.findElement(By.xpath("//a[text()='" + productName + "']/ancestor::div[@*='details" +
+                "']//input"));
+        product.click();
+    }
+
+
 }
